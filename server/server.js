c2d2a38f352376dc696cd2d471574e7ed717ec7c
Stevn <kasper@stevnbak.dk>
2022-11-11 14:51:34 +0100

    Server...

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const {Server} = require('socket.io');
 const io = new Server(server);
 const {Socket} = require('socket.io-client');
 
-///app.use(express.static('../site/public/'));
 app.use(express.static("../site/dist/"));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../site/dist/index.html'));
 });
