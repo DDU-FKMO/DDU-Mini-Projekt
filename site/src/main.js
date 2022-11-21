08f4a7e4e19cb2979313defba304714f3a35a845
Stevn <kasper@stevnbak.dk>
2022-11-21 14:58:38 +0100

    Server connection

--- site/src/main.js
+++ site/src/main.js
@@ -1,6 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {io} from 'socket.io-client';
 
-import './assets/main.css'
+//Connection with server
+export var IO = {
+    socket: io(),
+    init: function () {
+        IO.bindEvents();
+    },
 
-createApp(App).mount('body')
+    bindEvents: function () {
+        IO.socket.on('connected', IO.onConnected);
+        IO.socket.on('error', function (e) {
+            console.error(e);
+        });
+    },
+
+    onConnected: function () {
+        console.log("Succesfully connected to server");
+        console.log("Socket id: " + IO.socket.id);
+        IO.socket.emit("test");
+    },
+
+    error: function (data) {
+        alert('Something went wrong');
+    },
+};
+IO.init();
+
+
+//Mount App
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/main.css';
+
+createApp(App).mount('body');
+
+
