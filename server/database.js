fc5d72c03aad39c0f1d01bb0583ede0d00acc325
Filip <35558467+AntiMagneticGuy@users.noreply.github.com>
2022-11-23 15:57:22 +0100

    tilføjet database

--- server/database.js
+++ server/database.js
@@ -1,3 +1,12 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./users.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
+
 module.exports = {
     get: (table, id) => {
         
