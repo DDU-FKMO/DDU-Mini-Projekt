0a01935e5fe483a4d465eaaba0184c386fdd3dd7
Filip <35558467+AntiMagneticGuy@users.noreply.github.com>
2022-12-12 15:36:22 +0100

    klasse

--- site/src/components/shared/Klasse.vue
+++ site/src/components/shared/Klasse.vue
@@ -1,6 +1,37 @@
 <template>
-    Klasse 1
-</template> 
+    <div class="klasse">
+        <span>Navn: {{ klasseNavn }}</span>
+        <span>InviteCode: {{ inviteCode }}</span>
+    </div>
+</template>
 
+<script>
+export default {
+    name: "KlasseListe",
+    props: { klasseNavn: "", inviteCode: "" },
+    methods: {
 
+    },
+    mounted() {
 
+    }
+}
+</script>
+
+<style scoped>
+.klasse {
+    width: 20rem;
+    height: 10rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    border: 2px solid black;
+    padding: 2px;
+    margin: 2rem;
+}
+
+.klasse span {
+    font-size: 1.25rem;
+}
+</style>
--- site/src/components/shared/KlasseOverview.vue
+++ site/src/components/shared/KlasseOverview.vue
@@ -1,16 +1,32 @@
 <template>
     <h1>Klasser:</h1>
-    <button v-for="klasse in classes">{{klasse.className}}</button>
+    <div class="klasser" >
+        <Klasse v-for="klasse in klasser" :klasseNavn="klasse.className" :inviteCode="klasse.inviteCode"></Klasse>
+    </div>
 </template> 
 
 <script>
 //import { getUserInfo } from '../main.js';
+import Klasse from "./Klasse.vue";
 
 export default {
+    name: "KlasseOverview",
+    components: { Klasse },
     data: () => {
         return {
-            classes: [{ className: "3a2" }, { className: "3c" }, { className: "3h" }],
+            klasser: [{ className: "3a2", inviteCode: "123" }, { className: "3c", inviteCode: "test" }, { className: "3h", inviteCode: "3924ef" }],
         }
     }
 }
 </script>
+
+
+<style scoped>
+.klasser {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+</style>
