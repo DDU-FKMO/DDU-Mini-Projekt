0f42c55ccdff7e610430ee877563cb3613e97742
Filip <35558467+AntiMagneticGuy@users.noreply.github.com>
2022-12-20 11:16:04 +0100

    Update KlasseOverview.vue

--- site/src/components/shared/KlasseOverview.vue
+++ site/src/components/shared/KlasseOverview.vue
@@ -18,7 +18,7 @@ export default {
     data: () => {
         return {
             klasser: [
-                {className: '', inviteCode: ''},
+                
             ],
             userInfo: {},
         };
